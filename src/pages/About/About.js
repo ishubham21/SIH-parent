1a42ecc96df89bc5153a7ceb0e8f6ac805ff3fd8
payonidhi <payonidhi.20cse383@rtu.ac.in>
2022-08-12 11:51:38 +0530

    about

---
+++ src/pages/About/About.js
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import style from "./About.module.css";
+
+const About = () => {
+  useEffect(() => {
+    document.documentElement.style.setProperty(
+      "--navbar-button",
+      "#A390FB",
+    );
+  }, []);
+
+  return (
+    <>
+      <div className={style.about}>
+        <div className={style.navbar}>
+          <Navbar />
+        </div>
+        <div className={style.txt}>
+          <h1 className={style.heading}>About :</h1>
+          <p className={style.abtxt}>
+            RIVHEHOVR RGJRJGR3 FWROR GRWOFIWEF WEGFOIRWG WROFI EWFPOE
+            WQDPC QPOFE FPO S[PW DS POEDNE OPIOIQWNDE PIEF ECL QE
+            QFOVN EFOIVO QWDDL OQIE A ;KS C LJLD LLASS OIDND VODIVEW
+            WIDVND PIR ALJ NSIF 0EQ ID DJDD PRSKHET ID FI LOV
+            FWEVIOWNQEPFINF DIV EFPFIEEF EFPIF PRV EEFFOIV W
+          </p>
+        </div>
+
+        <div className={style.img_wrap}>
+          <img
+            src={require("../../assets/Saly-17.png")}
+            alt=" "
+            className={style.img}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default About;
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -2,12 +2,13 @@ import Dashboard from "../Dashboard/Dashboard";
 import Login from "../Login/Login";
 import Progress from "../Progress/Progress";
 import Signup from "../Signup/Signup";
+import About from "../About/About";
 import style from "./App.module.css";
 
 const App = () => {
   return (
     <>
-      <Progress />
+      <About />
     </>
   );
 };
